0f367627740b3364075be152c30ceba742845731
Pradeep Kumar <pradeepkumar0509@gmail.com>
2021-04-29 12:33:32 +0530

    Web frontend initial setup

---
+++ web-jsp/src/main/java/com/sportsbazaar/web/jsp/configuration/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.sportsbazaar.web.jsp.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.sportsbazaar")
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/views/", ".jsp");
+	}
+	
+}
---
+++ web-jsp/src/main/java/com/sportsbazaar/web/jsp/controller/AppController.java
@@ -0,0 +1,14 @@
+package com.sportsbazaar.web.jsp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+}
