c10553debff37585632455b596439e4f0de58db7
Pradeep Kumar <pradeepkumar0509@gmail.com>
2021-04-29 12:44:20 +0530

    Added logging framework

--- web-jsp/src/main/java/com/sportsbazaar/web/jsp/controller/AppController.java
+++ web-jsp/src/main/java/com/sportsbazaar/web/jsp/controller/AppController.java
@@ -1,13 +1,18 @@
 package com.sportsbazaar.web.jsp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
 
+	private static final Logger log = LoggerFactory.getLogger(AppController.class);
+
 	@RequestMapping("/")
 	public String index() {
+		log.info("returing 'index'");
 		return "index";
 	}
 	
