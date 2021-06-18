2df98357ba376ba4254708d59667640bc00f6baa
Pradeep Kumar <pradeepkumar0509@gmail.com>
2021-06-18 17:16:43 +0530

    Added pagination to products.jsp

--- web-jsp/src/main/java/com/sportsbazaar/web/jsp/controller/ProductsController.java
+++ web-jsp/src/main/java/com/sportsbazaar/web/jsp/controller/ProductsController.java
@@ -1,6 +1,7 @@
 package com.sportsbazaar.web.jsp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,14 @@ public class ProductsController {
 	private ProductService productService;
 	
 	@RequestMapping("/products")
-	public String productsList(Model model) {
-		model.addAttribute("productsList", this.productService.findAll());
+	public String productsList(@RequestParam(name = "page", required = false, defaultValue = "1") int page,
+			@RequestParam(name = "size", required = false, defaultValue = "5") int size, Model model) {
+		Page<Product> paged = this.productService.findPaginated(page, size);
+		model.addAttribute("productsList", paged.getContent());
+		// For pagination
+		model.addAttribute("currentPage", page);
+		model.addAttribute("totalPages", paged.getTotalPages());
+		model.addAttribute("totalElements", paged.getTotalElements());
 		return "products";
 	}
 	
